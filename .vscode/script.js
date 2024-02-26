9b24bcfe843873846f2b03f90f2951c36128ef61
Daniel Filho <dandanfilhopedrosa12@gmail.com>
2024-02-26 19:57:13 -0300

    first commit

---
+++ .vscode/script.js
@@ -0,0 +1,15 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // subtituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", './assets/avatar-light2.png')
+  } else {
+    // se tiver light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
